a7d22346f693d8ddfbee56054f6ed201f731c15a
will_huang <will_huang@trend.com.tw>
2016-12-07 21:42:08 +0800

    Completed

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -20,19 +20,50 @@ class APIManager {
         
         let url = URL(string: urlString)!
         
+//        let task = session.dataTask(with: url, completionHandler: {
+//            (data, response, error) -> Void in
+//            
+//            DispatchQueue.main.async {
+//                if error != nil {
+//                    completion((error?.localizedDescription)!)
+//                }else {
+//                    completion("URLSession successful!")
+//                    print(data)
+//                }
+//            }
+//            
+//           
+//        })
+//        
+//        task.resume()
+        
         let task = session.dataTask(with: url, completionHandler: {
             (data, response, error) -> Void in
             
-            DispatchQueue.main.async {
-                if error != nil {
+            if error != nil {
+                DispatchQueue.main.async {
                     completion((error?.localizedDescription)!)
-                }else {
-                    completion("URLSession successful!")
-                    print(data)
+                    
+                }
+            }else{
+                print(data)
+                do{
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: AnyObject]{
+                        print(json)
+                        
+                        DispatchQueue.global(qos: .userInitiated).async {
+                            DispatchQueue.main.async {
+                                completion("JSONSerialization Successful")
+                            }
+                        }
+                        
+                    }
+                }catch{
+                    DispatchQueue.main.async {
+                        completion("error in JSONSerialization")
+                    }
                 }
             }
-            
-           
         })
         
         task.resume()
